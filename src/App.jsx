bbad18c01f32bfe7479184132b90e198e86e8f16
Ayush0115D <dhakreayush578@gmail.com>
2025-03-01 16:56:49 +0530

    added dependencies and data for password

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
+import { useState,useCallback } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [length ,setLength]=useState(8)
+const [numberAllowed,setNumberAllowed]=useState(false)
+const [charAllowed,setCharAllowed]=useState(false)
+const[password,setPassword]=useState("")
+const passwordGenerator= useCallback(()=>{},
+[length,numberAllowed,charAllowed,setPassword])
+let pass=""
+let str="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+if(numberAllowed)ste+="0123456789"
+if (charAllowed)str+=" ! @ # $ % ^ & * - _ = + \ | ; : / ? . >"
+for(let i=1;i<=Array.length;i++){
+  let char=Math.floor(Math.random().str.length+1)
+}
   return (
     <>
       
-      
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
     </>
   )
 }
