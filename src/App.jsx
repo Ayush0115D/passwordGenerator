99362584e4dd7e429981952268b6580a01688869
Ayush0115D <dhakreayush578@gmail.com>
2025-03-03 18:36:42 +0530

    added more hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-import { useState,useCallback } from 'react'
+import { useState,useCallback,useEffect,useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [length ,setLength]=useState(8)
+  const [length,setLength]=useState(8)
 const [numberAllowed,setNumberAllowed]=useState(false)
 const [charAllowed,setCharAllowed]=useState(false)
 const[password,setPassword]=useState("")
@@ -12,13 +12,17 @@ let pass=""
 let str="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
 if(numberAllowed)ste+="0123456789"
 if (charAllowed)str+=" ! @ # $ % ^ & * - _ = + \ | ; : / ? . >"
-for(let i=1;i<=array.length;i++){
-  let char=Math.floor(Math.random().str.length+1)
-  pass=str.charAt(char)
+for(let i=1;i<=length;i++){
+  let char=Math.floor(Math.random()*str.length+1)
+  pass+=str.charAt(char)
 }
 setPassword(pass)
 },
-[length,numberAllowed,charAllowed,setPassword])
+[length,numberAllowed,charAllowed,setPassword,])
+useEffect(()=>{
+  passwordGenerator()
+},
+[length,numberAllowed,charAllowed,setPassword,passwordGenerator])
 
   return (
     <>
@@ -36,14 +40,37 @@ readOnly/>
 <div className='flex text-sm gap-x-2'>
       <div className='flex items-center gap-x-1'>
         <input type="range"
-        min={7}
+        min={17}
         max={100}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e)=>{setLength (e.target.value)}}
          />
+         <label> Length:{length} </label>
       </div>
- </div> 
+      <div className=' flex items-center gap-x-1'></div>
+      <input type="checkbox"
+        defaultChecked={numberAllowed}
+        id="numberInput"
+        onChange={()=>{
+setNumberAllowed((prev)=> !prev);
+          }  }     
+         />
+         <label htmlFor="numberInput">Numbers</label>
+ <div className=' flex items-center gap-x-1'>
+      <input type="checkbox"
+        defaultChecked={charAllowed}
+        id="characterInput"
+        onChange={()=>{
+setCharAllowed((prev)=> !prev); 
+//ye callback use ni kroge to prev value ka access ni rhega..and ek baar true hogya to wahi rhega no.allow always
+          }  }     
+        />
+        <label htmlFor="characterInput">Characters</label>
  </div>
+ </div>
+ </div> 
 
- 
     </>
   )
 }
